49cdc7776ec8071e6405cf462cf179c06b22b0a4
Nitiphat2 <moozknot@gmail.com>
2021-03-04 15:45:01 +0700

    พัก

--- 6320502410_lab4_2.c
+++ 6320502410_lab4_2.c
@@ -1,42 +1,48 @@
 #include<stdio.h>
 #include<string.h>
+#include<math.h>
 int main()
 {
-    int n,i,j,l;
+    int n,i,j,l,a=0;
     scanf("%d",&n);
-    char num[n][n];
+    char num[n],s[n];
     int m[n];
     for(i=0;i<n;i++)
     {
-        m[i]=0;
-        scanf("%c",&num[i]);
-        j=strlen(num[i]);
+        m[a]=0;
+        scanf(" %s",&num);
+        j=strlen(num);
         if(j==1)
         {
-            if(num[i]=='+')
+            if(strcmp(num,"+"))
             {
-
+                m[a-2]=m[a-2]+m[a-1];
+                a--;
             }
-            else if(num[i]=='-')
+            else if(strcmp(num,"-"))
             {
-
+                m[a-2]=m[a-2]-m[a-1];
+                a--;
             }
-            else if(num[i]=='*')
+            else if(strcmp(num,"*"))
             {
-
+                m[a-2]=m[a-2]*m[a-1];
+                a--;
             }
             else
             {
-
+                m[a]=m[a]+((int)num[0]-48);
+                a++;
             }
         }
         else
         {
-            for(l=1;l<=j;l++)
+            for(l=0;l<j;l++)
             {
-                m[i]=m[i]
+                m[a] = m[a]+(((int)num[l] - 48) * pow(10,j-l));
             }
+            a++;
         }
-
     }
+    printf("%d",m[a-1]);
 }
