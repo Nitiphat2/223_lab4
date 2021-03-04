e7578864f2ecf1899f20716a06887afeb9a0d58a
Nitiphat2 <moozknot@gmail.com>
2021-03-04 16:56:41 +0700

    งง

--- 6320502410_lab4_2.c
+++ 6320502410_lab4_2.c
@@ -7,42 +7,38 @@ int main()
     scanf("%d",&n);
     char num[n],s[n];
     int m[n];
-    for(i=0;i<n;i++)
+    for(i=0; i<n; i++)
     {
         m[a]=0;
         scanf(" %s",&num);
         j=strlen(num);
-        if(j==1)
+
+        if(!strcmp(num,"+"))
         {
-            if(!strcmp(num,"+"))
-            {
-                m[a-2]=m[a-2]+m[a-1];
-                a--;
-            }
-            else if(!strcmp(num,"-"))
-            {
-                m[a-2]=m[a-2]-m[a-1];
-                a--;
-            }
-            else if(!strcmp(num,"*"))
-            {
-                m[a-2]=m[a-2]*m[a-1];
-                a--;
-            }
-            else
-            {
-                m[a]=m[a]+((int)num[0]-48);
-                a++;
-            }
+            m[a-2]=m[a-2]+m[a-1];
+            a--;
+        }
+        else if(!strcmp(num,"-"))
+        {
+            m[a-2]=m[a-2]-m[a-1];
+            a--;
+        }
+        else if(!strcmp(num,"*"))
+        {
+            m[a-2]=m[a-2]*m[a-1];
+            a--;
         }
         else
         {
-            for(l=0;l<j;l++)
+            for(l=0; l<j; l++)
             {
-                m[a] = m[a]+(((int)num[l] - 48) * pow(10,j-l-1));
+                m[a] = m[a]+((num[l] - 48) * pow(10,j-l-1));
             }
             a++;
         }
     }
-    printf("%d",m[a-1]);
+    for(i=0;i<a;i++)
+    {
+        printf("%d\n",m[i]);
+    }
 }
