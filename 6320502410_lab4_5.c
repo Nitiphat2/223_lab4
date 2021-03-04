2c0c964ead6ceb1c475bb0fba6de02b15feeea92
Nitiphat2 <moozknot@gmail.com>
2021-03-04 16:29:37 +0700

    wow

---
+++ 6320502410_lab4_5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    int n,j,i,k;
+    scanf("%d",&n);
+    char play[50];
+    for(i=0;i<n;i++)
+    {
+        scanf(" %s",play);
+        k=strlen(play);
+        for(j=0;j<k;j++)
+        {
+            if(play[j]=='A')
+            {
+
+            }
+            else if(play[j]=='B')
+            {
+
+            }
+            else if(play[j]=='C')
+            {
+
+            }
+            else if(play[j]=='D')
+            {
+
+            }
+            else if(play[j]=='E')
+            {
+
+            }
+        }
+    }
+}
