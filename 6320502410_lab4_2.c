2378ec2f0066669ad4b58bc862e0b9c3e275246d
Nitiphat2 <moozknot@gmail.com>
2021-03-04 15:14:25 +0700

    555

--- 6320502410_lab4_2.c
+++ 6320502410_lab4_2.c
@@ -2,28 +2,41 @@
 #include<string.h>
 int main()
 {
-    int n,i;
+    int n,i,j,l;
     scanf("%d",&n);
     char num[n][n];
     int m[n];
     for(i=0;i<n;i++)
     {
+        m[i]=0;
         scanf("%c",&num[i]);
-        if(num[i]=='+')
+        j=strlen(num[i]);
+        if(j==1)
         {
+            if(num[i]=='+')
+            {
 
-        }
-        else if(num[i]=='-')
-        {
+            }
+            else if(num[i]=='-')
+            {
 
-        }
-        else if(num[i]=='*')
-        {
+            }
+            else if(num[i]=='*')
+            {
 
+            }
+            else
+            {
+
+            }
         }
         else
         {
-
+            for(l=1;l<=j;l++)
+            {
+                m[i]=m[i]
+            }
         }
+
     }
 }
