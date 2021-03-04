bf8286eeb420ba6972d2893c2d7e432e6b2e5597
Nitiphat2 <moozknot@gmail.com>
2021-03-04 15:09:12 +0700

    ยาก

--- 6320502410_lab4_2.c
+++ 6320502410_lab4_2.c
@@ -1,2 +1,29 @@
 #include<stdio.h>
+#include<string.h>
 int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    char num[n][n];
+    int m[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%c",&num[i]);
+        if(num[i]=='+')
+        {
+
+        }
+        else if(num[i]=='-')
+        {
+
+        }
+        else if(num[i]=='*')
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
