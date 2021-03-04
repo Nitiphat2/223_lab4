a330d7776b4ad44f70760b828218ec4ba2a342e8
Nitiphat2 <moozknot@gmail.com>
2021-03-04 14:41:54 +0700

    lab1_2

--- 6320502410_lab4_1.c
+++ 6320502410_lab4_1.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
 int main()
 {
+    int n;
+    scanf("%d",&n);
+    int a[n],b[n],c[n];
+    int i,j,k;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d%d",&a[i],&b[i],&c[i]);
+    }
 
 }
