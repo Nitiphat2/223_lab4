d4b2a11289730d0057bb08f42b4aa3449e2e6130
Nitiphat2 <moozknot@gmail.com>
2021-03-04 15:54:33 +0700

    5555

--- 6320502410_lab4_3.c
+++ 6320502410_lab4_3.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
     char name[200];
     gets(name);
-    puts(name);
+    int i,j=0,k;
+    i=strlen(name);
+    while(j<i)
+    {
+        if(name[j-1]!=name[j])
+        {
+            printf("%c",name[j]);
+        }
+        j++;
+    }
 }
