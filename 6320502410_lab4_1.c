284c5fb7760ec9202fd3d725cf0fb70fc6929f1e
Nitiphat2 <moozknot@gmail.com>
2021-03-04 14:51:30 +0700

    lab1_3

--- 6320502410_lab4_1.c
+++ 6320502410_lab4_1.c
@@ -3,11 +3,34 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    int a[n],b[n],c[n];
+    int a[n],b[n],c[n],sum[n];
     int i,j,k;
     for(i=0;i<n;i++)
     {
         scanf("%d%d%d",&a[i],&b[i],&c[i]);
     }
-
+    for(i=0;i<n;i++)
+    {
+        sum[i]=(a[i]*4)+(b[i]*2)+(c[i]*1);
+    }
+    int m[n-2];
+    for(i=0;i<n-2;i++)
+    {
+        m[i]=sum[i]+sum[i+1]+sum[i+2];
+    }
+    int max=m[0],less=m[0];
+    for(i=0;i<n-2;i++)
+    {
+        if(max < m[i])
+        {
+            max = m[i];
+            j=i+1;
+        }
+        if(less > m[i])
+        {
+            less = m[i];
+            k=i+1;
+        }
+    }
+    printf("%d %d",j,k);
 }
